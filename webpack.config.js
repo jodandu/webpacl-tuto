9237609ae0d8c9e5202274b1a7ff1eb29069b9e8
Josue Duran <josue.duran@globant.com>
2016-08-02 12:30:05 -0500

    fisrt-commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: ['./global.js', './app.js'],
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'jshint-loader'
+
+    }],
+    loaders: [{
+      test: [/\.js$/, /\.es6$/],
+      exclude: 'node_modules',
+      loader: 'babel-loader',
+      query: {
+        cacheDirectory: true,
+        presets: ['es2015']
+      }
+
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.es6']
+  }
+};
